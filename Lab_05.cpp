cdd117a089e4731c565b225e06720c5bdb16c4de
Shefat Al Mahmud <shefat2002@gmail.com>
2022-08-31 10:23:59 +0600

    Create Lab_05.cpp

---
+++ Lab_05.cpp
@@ -0,0 +1,10 @@
+/*
+Pointers to impliment different operations in one-dimensional array
+1. Summation of element
+2. Counting the odd and even number
+3. finding the multiple of 5 and 10
+4. Linear Search
+5. Binary Search
+6. Bubble Sort
+7. K-th Maximum and K-th minimum
+*/
