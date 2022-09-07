8460bc0cb15fd0eaac5296770224f30aee5b4103
Shefat Al Mahmud <shefat2002@gmail.com>
2022-09-07 11:09:54 +0600

    Create Lab_06.cpp

---
+++ Lab_06.cpp
@@ -0,0 +1,12 @@
+/*
+.......using pointer
+
+1. Write a program uing pointers to pergorm the following tasts.
+2. To compute the summation of numbers in an one-dimensional array.
+3. To search an element using linear search
+4. To compute the frequency of a given number
+5. To compute the summation of diagonal and non diagonal number of a two dimensional array
+6. To find the k-th maximum and k-th minimum.
+7. To compute standard derivation of the numbers stroed in an array.
+8. Insertion and deletion.
+*/
